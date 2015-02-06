1b064b446d0bc03a454c9e8cccd589005eb43de3
Ray Grasso <ray.grasso@gmail.com>
2015-02-06 16:51:28 +0800

    Comments

--- public/js/lib/main.jsx
+++ public/js/lib/main.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import Widgets from "./widgets";
 // import Router from "react-router";
 
+// Old style component
 var WidgetList = React.createClass({
-
   render() {
     var widgets = this.props.widgets.map(widget => {
       return <Widget widget={ widget } key= { widget.id } />;
@@ -13,7 +13,7 @@ var WidgetList = React.createClass({
   }
 });
 
-
+// ES6 class component
 class Widget extends React.Component {
   render() {
     return <li>{this.props.widget.name}</li>;
